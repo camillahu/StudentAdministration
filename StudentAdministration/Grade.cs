5e14a449c1cb67f4984eec5954132180a5ae7837
camillahu <camillahaugenurdal@gmail.com>
2024-05-20 12:29:18 +0200

    Add project files.

---
+++ StudentAdministration/Grade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAdministration
+{
+    internal class Grade
+    {
+        public Student Student { get; private set; }
+        public Subject Subject { get; private set; }
+        public int Value { get; private set; }
+
+        public Grade(Student student, Subject subject, int value)
+        {
+            Student = student;
+            Subject = subject;
+            Value = value;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Grade: \n" +
+                              $"Student: {Student.Name}\n" +
+                              $"Subject: {Subject.Name}\n" +
+                              $"Grade value: {Value}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StudentAdministration/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAdministration
+{
+    internal class Student
+    {
+        public string Name { get;  private set; }
+        public int Age { get; private set; }
+        public string Program { get;  private set; }
+        public int Id { get; private set; }
+
+        public List <Subject> Subjects { get; private set; }
+
+        public Student(string name, int age, string program, int id)
+        {
+            Name = name; 
+            Age = age;
+            Program = program;
+            Id = id;
+            Subjects = new List<Subject>();
+        }
+
+        public void AddSubject(Subject subject)
+        {
+            Subjects.Add(subject);
+        }
+        public void PrintInfo() 
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Student:\n" +
+                                $"Name: {Name} \n" +
+                                $"Age: {Age} \n" +
+                                $"Program: {Program} \n" +
+                                $"Id: {Id}");
+            Console.WriteLine();
+        }
+    }
+}
